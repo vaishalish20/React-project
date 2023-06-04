9d473e97dd1ccf858ca932e3ca22b02cbb0eb5ee
Vaishali <vaishalisharmaukl@gmail.com>
2023-06-04 09:37:29 +0530

    final-try

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://vaishalish20.github.io/react-project/",
+  base: "/react-project",
   plugins: [react()],
 })
