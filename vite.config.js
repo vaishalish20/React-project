4c3f034d81e2563098a89d0831ff02b7da9971de
Vaishali <vaishalisharmaukl@gmail.com>
2023-06-04 09:44:05 +0530

    trying

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/react-project",
-  plugins: [react()],
-})
+// export default defineConfig({
+//   base: "/react-project",
+//   plugins: [react()],
+// })
+export default {
+  build: {
+    base: '/https://github.com/vaishalish20/react-project/'
+  }
+}
